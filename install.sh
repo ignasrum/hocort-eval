107b11ef5a3f4bc14f54db51451d53b016c07baf
ignasrum <rumbavicius@gmail.com>
2022-03-06 22:00:18 +0000

    updated dependencies and installation process

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+
+conda env create -f environment.yml
+conda activate hocort-eval && 
+pip install git+https://github.com/ignasrum/hocort.git@0.3.0 &&
+pip install git+https://github.com/ignasrum/fastq-tools.git@0.1.0
