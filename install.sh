293293fe94d0c2a51ec35ed92711b6f3ab50cb9e
ignasrum <rumbavicius@gmail.com>
2022-05-13 21:59:15 +0200

    bumped hocort version

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ source $CONDA_BASE/etc/profile.d/conda.sh
 
 conda env create -f environment.yml
 conda activate hocort-eval && 
-pip install git+https://github.com/ignasrum/hocort.git@0.3.3 &&
+pip install git+https://github.com/ignasrum/hocort.git@1.0.0 &&
 pip install git+https://github.com/ignasrum/fastq-tools.git@0.1.0
